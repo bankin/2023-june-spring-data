ade2660d78a767040bd956a4beb111609a8b7a94
Nikolay Bankin <niky.bankin@gmail.com>
2023-06-21 07:10:42 +0300

    Add Hibernate Intro files

---
+++ 07_ClassicHibernate/src/main/java/Main.java
@@ -0,0 +1,33 @@
+import entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sessionFactory =
+                cfg.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Student student = new Student("Pesho2", 15);
+        session.save(student);
+
+        Student student1 = session.get(Student.class, 3);
+        System.out.println(student1);
+
+        List<Student> allStudents = session
+            .createQuery("FROM Student AS s WHERE s.name='Pesho2'", Student.class)
+            .list();
+
+        allStudents.forEach(System.out::println);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+}
---
+++ 07_JPAHibernate/src/main/java/Main.java
@@ -0,0 +1,35 @@
+import entities.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("hibernate-jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Student student = new Student("TeoNew", 10, "School 1");
+        em.persist(student);
+//
+//        student.setName("TeoUpdated");
+//        em.persist(student);
+
+//        Student findStudent = em.find(Student.class, 5);
+//        em.remove(findStudent);
+//        System.out.println(findStudent.getId());
+
+        List<Student> from_student = em
+            .createQuery("FROM Student", Student.class)
+            .getResultList();
+
+        from_student.forEach(s -> System.out.println(s));
+
+        em.getTransaction().commit();
+    }
+}
